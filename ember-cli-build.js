bf31150bef6e20d08497d54633f7409f8a03628d
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-03-26 21:48:00 +0100

    add ember with appuniversum

---
+++ ember-cli-build.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp({
+    sassOptions: {
+      includePaths: ['node_modules/@appuniversum/ember-appuniversum'], // just "node_modules" would also work, but it seems to slow (re)builds down a lot
+    },
+  });
+
+  return app.toTree();
+};
---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'frontend-redirect-poc/app';
+import config from 'frontend-redirect-poc/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();

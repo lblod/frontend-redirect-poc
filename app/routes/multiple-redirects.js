280303b127febbc952e185c3dac3ca05578ac9ff
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-04-08 22:33:52 +0200

    use AuLinkExternal component to show urls

--- app/routes/multiple-redirects.js
+++ app/routes/multiple-redirects.js
@@ -5,10 +5,15 @@ export default class MultipleRedirectsRoute extends Route {
   @service router;
 
   model() {
+    const { redirectUrlArray, resource } = this.redirectDataService;
     if (this.redirectDataService.redirectUrlArray.length > 0) {
-      return this.redirectDataService.redirectUrlArray;
+      const processedUrls = redirectUrlArray.map((url) =>
+        url.replace('$resource', encodeURIComponent(resource))
+      );
+      return processedUrls;
     } else {
       this.router.transitionTo('not-found');
+      return [];
     }
   }
 }
